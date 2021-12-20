2aa64ae0224a3027fc5b8289bf12e2a3cf2af1a1
Jack Guinane <jackguinane@gmail.com>
2021-12-19 23:41:43 -0800

    Update settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,1 +1,20 @@
-export default ({ renderers: []})
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
+
+
+// This is to get Puppeteer (and ultimately capture-website) working for the
+// social-image-generator.js script.
+const ignoredPackages = [
+	'node:process', 'node:fs',
+	'file-url',
+	'puppeteer',
+	'tough-cookie', 
+]
+
+export default ({
+	renderers: [],
+	vite: {
+		optimizeDeps: { exclude: ignoredPackages },
+		ssr: { external: ignoredPackages },
+		plugins: [ viteCommonjs() ],
+	},
+})
